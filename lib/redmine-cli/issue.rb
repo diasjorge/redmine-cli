2075f92057ba3e7701b350c3d25034da85ed6b65
Ben Hawkins <ben.hawkins@noa.nintendo.com>
2014-03-06 17:25:57 -0800

    This is a fix for https://github.com/diasjorge/redmine-cli/issues/2 . Redmine includes additional attributes in <issues> element that confuse ActiveResource. We can scrub them for our purposes.  Workaround was copied from http://community.pivotaltracker.com/pivotal/topics/activeresource_client_throws_error .

--- lib/redmine-cli/issue.rb
+++ lib/redmine-cli/issue.rb
@@ -20,3 +20,16 @@ module Redmine
     end
   end
 end
+
+class Hash
+  class << self
+    alias_method :from_xml_original, :from_xml
+    def from_xml(xml)
+      scrubbed = scrub_attributes(xml)
+      from_xml_original(scrubbed)
+    end
+    def scrub_attributes(xml)
+      xml.gsub(/<issues .*?>/, "<issues type=\"array\">")
+    end
+  end
+end
